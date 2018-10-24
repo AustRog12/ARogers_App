c2edff5e21d5ca1e1ece2c149669a625f6ed292c
0705769 <0705769@KELCE-ACIS08-P>
2018-10-24 17:55:44 -0500

    Added Skills page and will format AboutMe page later

---
+++ ARogers_App/ARogers_App/SkillsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ARogers_App
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SkillsPage : ContentPage
+	{
+        public ObservableCollection<string> Skills { get; set; }
+		public SkillsPage ()
+		{
+			InitializeComponent ();
+
+            this.Skills = new ObservableCollection<string>();
+
+		}
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            this.Skills.Add(skillEntry.Text);
+
+            skillEntry.Text = "";
+
+            skillsListView.ItemsSource = this.Skills;
+        }
+    }
+}
